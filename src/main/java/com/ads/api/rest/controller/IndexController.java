2376f97bd1967b02f87896c4c2d804c7ee664caa
Adson Conceição <adsonconcecao@yahoo.com.br>
2021-12-07 23:18:53 -0300

    Post Usúario Cadastro

--- src/main/java/com/ads/api/rest/controller/IndexController.java
+++ src/main/java/com/ads/api/rest/controller/IndexController.java
@@ -8,6 +8,8 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -17,37 +19,59 @@ import com.ads.api.rest.repository.UsuarioRepository;
 @RestController
 @RequestMapping(value = "/usuario")
 public class IndexController {
-	
+
 	@Autowired
 	private UsuarioRepository userRepo;
-	
-	/*@GetMapping(value = "/{id/codigovenda/{venda}", produces = "application/pdf")
-	public ResponseEntity<Usuario> relatorio(@PathVariable (value = "id") Long id
-			, @PathVariable (value = "venda") Long venda) {
-	
-		Optional<Usuario> user = userRepo.findById(id);	
-	
-		// seria um retorno de relatório
-	return new ResponseEntity<Usuario>(user.get(),HttpStatus.OK);
-	}*/
-	
-	
+
+	/*
+	 * @GetMapping(value = "/{id/codigovenda/{venda}", produces = "application/pdf")
+	 * public ResponseEntity<Usuario> relatorio(@PathVariable (value = "id") Long id
+	 * , @PathVariable (value = "venda") Long venda) {
+	 * 
+	 * Optional<Usuario> user = userRepo.findById(id);
+	 * 
+	 * // seria um retorno de relatório return new
+	 * ResponseEntity<Usuario>(user.get(),HttpStatus.OK); }
+	 */
+
 	@GetMapping(value = "/{id}", produces = "application/json")
-	public ResponseEntity<Usuario> init(@PathVariable (value = "id") Long id) {
-	
-		Optional<Usuario> user = userRepo.findById(id);	
-		
-	return new ResponseEntity<Usuario>(user.get(),HttpStatus.OK);
+	public ResponseEntity<Usuario> init(@PathVariable(value = "id") Long id) {
+
+		Optional<Usuario> user = userRepo.findById(id);
+
+		return new ResponseEntity<Usuario>(user.get(), HttpStatus.OK);
 	}
-	
+
 	@GetMapping(value = "/", produces = "application/json")
-	public ResponseEntity<List<Usuario>> user () {
-		
-	List<Usuario> list = (List<Usuario>) userRepo.findAll();
-		
+	public ResponseEntity<List<Usuario>> user() {
+
+		List<Usuario> list = (List<Usuario>) userRepo.findAll();
+
 		return new ResponseEntity<List<Usuario>>(list, HttpStatus.OK);
-		
+
 	}
-	
-	
+
+	@PostMapping(value = "/", produces = "application/json")
+	public ResponseEntity<Usuario> salvar(@RequestBody Usuario user) {
+
+		Usuario userSalvo = userRepo.save(user);
+
+		return new ResponseEntity<Usuario>(userSalvo, HttpStatus.OK);
+	}
+
+	/*
+	 * @SuppressWarnings({ "unchecked", "rawtypes" })
+	 * 
+	 * @PostMapping(value = "/{iduser}/idvenda/{idvenda}", produces =
+	 * "application/json") public ResponseEntity<?> salvarVendas(@PathVariable Long
+	 * idsuer,
+	 * 
+	 * @PathVariable Long idvenda) {
+	 * 
+	 * Aqui processar vendas //Usuario userSalvo = userRepo.save(user);
+	 * 
+	 * return new ResponseEntity("id user :" + idsuer + "idvenda :" + idvenda,
+	 * HttpStatus.OK); }
+	 */
+
 }
