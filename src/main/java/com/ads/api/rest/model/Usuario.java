54f821b8f738618a6388ec19f61ea37e1dfee85d
Adson Conceição <adsonconcecao@yahoo.com.br>
2021-12-15 00:33:14 -0300

    Security: Config Usuario

--- src/main/java/com/ads/api/rest/model/Usuario.java
+++ src/main/java/com/ads/api/rest/model/Usuario.java
@@ -1,85 +1,101 @@
 package com.ads.api.rest.model;
 
-import java.io.Serializable;
 import java.util.ArrayList;
+import java.util.Collection;
 import java.util.List;
 import java.util.Objects;
 
 import javax.persistence.CascadeType;
+import javax.persistence.ConstraintMode;
 import javax.persistence.Entity;
+import javax.persistence.FetchType;
+import javax.persistence.ForeignKey;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.JoinTable;
 import javax.persistence.OneToMany;
+import javax.persistence.UniqueConstraint;
+
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
 
 @Entity
-public class Usuario implements Serializable{
+public class Usuario implements UserDetails {
 	private static final long serialVersionUID = 1L;
-	
+
 	@Id
 	@GeneratedValue(strategy = GenerationType.AUTO)
 	private Long id;
-	
+
 	private String login;
-	
+
 	private String nome;
-	
+
 	private String senha;
-	
-	@OneToMany(mappedBy = "usuario" , orphanRemoval = true, cascade = CascadeType.ALL)
+
+	@OneToMany(mappedBy = "usuario", orphanRemoval = true, cascade = CascadeType.ALL, fetch = FetchType.LAZY)
 	private List<Telefone> telefones = new ArrayList<Telefone>();
-	
 
 	
-	public Usuario() {
+	@OneToMany(fetch = FetchType.EAGER)
+	@JoinTable(name = "usuarios_role", uniqueConstraints = @UniqueConstraint(
+			columnNames = {"usuario_id", "role_id"}, name = "unique_role_users"),
+	joinColumns = @JoinColumn(name = "usuario_id", referencedColumnName = "id", table = "usuario",
+	foreignKey = @ForeignKey(name = "usuario_fk", value = ConstraintMode.CONSTRAINT)),
+	inverseJoinColumns = @JoinColumn(name = "role_id", referencedColumnName = "id", table = "role",
+	foreignKey = @ForeignKey(name = "role")))
+	private List<Role> roles; /*Os papeis de acesso*/
+	
+	
+	public List<Telefone> getTelefones() {
+		return telefones;
 	}
 
+	public void setTelefones(List<Telefone> telefones) {
+		this.telefones = telefones;
+	}
+
+	public Usuario() {
+	}
 
 	public Long getId() {
 		return id;
 	}
 
-
 	public void setId(Long id) {
 		this.id = id;
 	}
-
 
 	public String getLogin() {
 		return login;
 	}
 
-
 	public void setLogin(String login) {
 		this.login = login;
 	}
-
 
 	public String getSenha() {
 		return senha;
 	}
 
-
 	public void setSenha(String senha) {
 		this.senha = senha;
 	}
-
 
 	public String getNome() {
 		return nome;
 	}
 
-
 	public void setNome(String nome) {
 		this.nome = nome;
 	}
-
 
 	@Override
 	public int hashCode() {
 		return Objects.hash(id);
 	}
-
 
 	@Override
 	public boolean equals(Object obj) {
@@ -93,16 +109,43 @@ public class Usuario implements Serializable{
 		return Objects.equals(id, other.id);
 	}
 
+	
 
-	public List<Telefone> getTelefones() {
-		return telefones;
+	/*São os acessos dos usuario Role_ADMIN ou ......*/
+	@Override
+	public Collection<? extends GrantedAuthority> getAuthorities() {
+		return null;
+	}
+
+	@Override
+	public String getPassword() {
+		return this.senha;
+	}
+
+	@Override
+	public String getUsername() {
+		return this.login;
+	}
+
+	@Override
+	public boolean isAccountNonExpired() {
+		return true;
+	}
+
+	@Override
+	public boolean isAccountNonLocked() {
+		return true;
+	}
+
+	@Override
+	public boolean isCredentialsNonExpired() {
+		return true;
+	}
+
+	@Override
+	public boolean isEnabled() {
+		return true;
 	}
 
 
-	public void setTelefones(List<Telefone> telefones) {
-		this.telefones = telefones;
-	}
-
-	
-	
 }
